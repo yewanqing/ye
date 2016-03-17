492407b6e48d69c8d996e6ee04534b9194d812d7
yewanqing <yewanqing@21tb.com>
2016-03-17 16:22:10 +0800

    Test 提交

--- src/main/java/mail/Test.java
+++ src/main/java/mail/Test.java
@@ -5,7 +5,7 @@ package mail;
  */
 public class Test {
     public static void main(String[] args) {
-        String str = "01|总行部门|";
+        String str = "00004925|张世林|00004925|男|0|91901|副行长|13826013694|2015-01-20 16:12:34|zhangshilin@gzrcu.cn|||||||||村镇银行条线|管理序列|正式行员|1983-04-21|在职博士研究生|常宁珠江村镇银行|1068NS10000000000035|副经理级";
         String[] split = str.split("\\|");
         for (String s : split) {
             System.out.println(s);
